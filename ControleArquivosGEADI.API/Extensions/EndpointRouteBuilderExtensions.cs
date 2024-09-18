835db41557c8cd734151b2fd38f7cfbd6d0d58a6
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-09-18 20:24:07 -0300

    feat: criação de extensões EndpointRouterBuilderExtensiosn criando Register específicos para os endpoint

---
+++ ControleArquivosGEADI.API/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ControleArquivosGEADI.API.EndpointHandlers;
+namespace ControleArquivosGEADI.API.Extensions;
+
+public static class EndpointRouteBuilderExtensions
+{
+    public static void RegisterArquivosEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var arquivosEndpoints = endpoints.MapGroup("/arquivos");
+        var arquivosComIdEndpoints = arquivosEndpoints.MapGroup("/{arquivoId:int}");
+        arquivosEndpoints.MapGet("", ArquivosHandlers.GetArquivosAsync);
+        arquivosComIdEndpoints.MapGet("", ArquivosHandlers.GetArquivosComIdAsync);
+    }
+
+    public static void RegisterLotesEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var lotesEndpoints = endpoints.MapGroup("/lotes");
+        var lotesComIdEndpoints = lotesEndpoints.MapGroup("/{loteId:int}");
+        lotesEndpoints.MapGet("", LotesHandlers.GetListasAsync);
+        lotesComIdEndpoints.MapGet("", LotesHandlers.GetListasComIdAsync).WithName("GetLotes");
+    }
+    public static void RegisterCapturasEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("/mapearpasta", CapturasHandlers.PostMapearArquivosAsync);
+        endpoints.MapPost("/etlbasemensal", CapturasHandlers.PostCapturaEtlBaseMensalAsync);
+    }   
+}
--- ControleArquivosGEADI.API/Program.cs
+++ ControleArquivosGEADI.API/Program.cs
@@ -1,13 +1,7 @@
-using CsvHelper;
-using CsvHelper.Configuration;
 using Microsoft.EntityFrameworkCore;
 using ControleArquivosGEADI.API.DbContexts;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http.HttpResults;
-using AutoMapper;
-using ControleArquivosGEADI.API.Models;
-using System.Globalization;
 using ControleArquivosGEADI.API.EndpointHandlers;
+using ControleArquivosGEADI.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,19 +19,11 @@ var app = builder.Build();
 
 //Criação do aplicativo
 
-app.MapGet("/", () => "Produção temática Elias Jácome - PSI MASTER GEADI - API C# NET8");
+//app.MapGet("/", () => "Produção temática Elias Jácome - PSI MASTER GEADI - API C# NET8");
 
-var arquivosEndpoints = app.MapGroup("/arquivos");
-var arquivosComIdEndpoints = arquivosEndpoints.MapGroup("/{arquivoId:int}");
-var lotesEndpoints = app.MapGroup("/lotes");
-var lotesComIdEndpoints = lotesEndpoints.MapGroup("/{loteId:int}");
-
-arquivosEndpoints.MapGet("", ArquivosHandlers.GetArquivosAsync);
-arquivosComIdEndpoints.MapGet("", ArquivosHandlers.GetArquivosComIdAsync);
-lotesEndpoints.MapGet("", LotesHandlers.GetListasAsync);
-lotesComIdEndpoints.MapGet("", LotesHandlers.GetListasComIdAsync).WithName("GetLotes");
-app.MapPost("/mapearpasta", CapturasHandlers.PostMapearArquivosAsync);
-app.MapPost("/etlbasemensal", CapturasHandlers.PostCapturaEtlBaseMensalAsync);
+app.RegisterArquivosEndpoints();
+app.RegisterLotesEndpoints();
+app.RegisterCapturasEndpoints();
 
 app.UseSwagger();
 app.UseSwaggerUI();
