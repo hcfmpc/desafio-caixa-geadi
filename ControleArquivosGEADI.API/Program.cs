d3a0e50cabdb31b404a33354f1a341330a6c46ce
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-09-19 22:35:30 -0300

    refator: remoção do método anterior de handler do exception, mantendo apenas o via services

--- ControleArquivosGEADI.API/Program.cs
+++ ControleArquivosGEADI.API/Program.cs
@@ -21,16 +21,6 @@ var app = builder.Build();
 if(!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler();
-    //app.UseExceptionHandler(configureApplicationBuilder =>
-    //{
-    //    configureApplicationBuilder.Run(
-    //    async context =>
-    //    {
-    //        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-    //        context.Response.ContentType = "text/html";
-    //        await context.Response.WriteAsync("An unexpected problem happened.");
-    //    });
-    //});
 }
 
 app.RegisterArquivosEndpoints();
