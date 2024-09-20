fb44c603c66bcea8154cb7703dec07358643e4e5
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-09-19 22:41:58 -0300

    feat: inclusão de log Ilooger nos Handlers para exibir no controlador informações

--- ControleArquivosGEADI.API/EndpointHandlers/ArquivosHandlers.cs
+++ ControleArquivosGEADI.API/EndpointHandlers/ArquivosHandlers.cs
@@ -12,6 +12,7 @@ public class ArquivosHandlers
     public static async Task<Results<NoContent, Ok<IEnumerable<ArquivoDTO>>>> GetArquivosAsync
     (ControleDboContext controleDboContext,
     IMapper mapper,
+    ILogger<ArquivoDTO> logger,
     [FromQuery(Name = "nome")]string? arquivoNome)
     {
         var arquivos = await controleDboContext.Aditb001ControleArquivos
@@ -19,9 +20,15 @@ public class ArquivosHandlers
                                                 .ToListAsync();
 
         if (arquivos.Count <= 0 || arquivos == null)
+        {
+            logger.LogInformation($"Arquivos não encontrados. Parâmetro: {arquivoNome}");
             return TypedResults.NoContent();
+        }
         else
+        {
+            logger.LogInformation("Retornando arquivos localizados");
             return TypedResults.Ok(mapper.Map<IEnumerable<ArquivoDTO>>(arquivos));
+        }
     }
 
     public static async Task<Ok<ArquivoDTO>> GetArquivosComIdAsync(
