b6ee9598873bf0dc6b6473a4c874762789e84d6f
Elias de Almeida Jacome Filho <c079795@corp.caixa.gov.br>
2024-09-19 22:34:07 -0300

    feat: teste de exceptions no modo production

--- ControleArquivosGEADI.API/Program.cs
+++ ControleArquivosGEADI.API/Program.cs
@@ -14,21 +14,23 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 if(!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler(configureApplicationBuilder =>
-    {
-        configureApplicationBuilder.Run(
-        async context =>
-        {
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            context.Response.ContentType = "text/html";
-            await context.Response.WriteAsync("An unexpected problem happened.");
-        });
-    });
+    app.UseExceptionHandler();
+    //app.UseExceptionHandler(configureApplicationBuilder =>
+    //{
+    //    configureApplicationBuilder.Run(
+    //    async context =>
+    //    {
+    //        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+    //        context.Response.ContentType = "text/html";
+    //        await context.Response.WriteAsync("An unexpected problem happened.");
+    //    });
+    //});
 }
 
 app.RegisterArquivosEndpoints();
